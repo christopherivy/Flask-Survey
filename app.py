7bd82b7a04f09ab85aff1f9a2cb9e5fd1944b9d8
christopherivy <cdi.747@gmail.com>
2021-08-17 15:25:25 -0700

    flask-survey3

--- app.py
+++ app.py
@@ -8,20 +8,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
+satis = surveys['satisfaction']
+choice = satis.questions[0].choices
+
+# this will keep track of user responses
+responses = []
 
 
 @app.route('/')
 def survey():
-    satis = surveys['satisfaction']
-
     return render_template("home.html", satis=satis)
 
 
 @app.route('/questions/0', methods=["POST"])
 def questions():
     return render_template("base.html", satis=satis)
-
-
-# this will keep track of user responses
-responses = []
-x = 'hello'
